d1a68d64fea6683738c1388a206a52f9fd291ae8
Tom Anderson <tom@ando.gq>
2023-10-17 21:56:14 +1100

    Get WASM working

--- wgpu-renderer/src/lib.rs
+++ wgpu-renderer/src/lib.rs
@@ -1,14 +1,44 @@
+use cfg_if::cfg_if;
+#[cfg(target_arch = "wasm32")]
+use wasm_bindgen::prelude::*;
+
 use winit::{
     event::*,
     event_loop::{ControlFlow, EventLoop},
     window::WindowBuilder,
 };
 
+#[cfg_attr(target_arch = "wasm32", wasm_bindgen(start))]
 pub fn run() {
-    env_logger::init();
+    cfg_if! {
+        if #[cfg(target_arch = "wasm32")] {
+            std::panic::set_hook(Box::new(console_error_panic_hook::hook));
+            console_log::init_with_level(log::Level::Warn).expect("Couldn't initialise logger");
+        } else {
+            env_logger::init();
+        }
+    }
 
     let event_loop = EventLoop::new();
     let window = WindowBuilder::new().build(&event_loop).unwrap();
+
+    #[cfg(target_arch = "wasm32")]
+    {
+        use winit::dpi::PhysicalSize;
+        use winit::platform::web::WindowExtWebSys;
+
+        window.set_inner_size(PhysicalSize::new(450, 400));
+
+        web_sys::window()
+            .and_then(|window| window.document())
+            .and_then(|document| document.body())
+            .and_then(|body| {
+                let canvas = web_sys::Element::from(window.canvas());
+                body.append_child(&canvas).ok()?;
+                Some(())
+            })
+            .expect("couldn't append child to document body");
+    }
 
     event_loop.run(move |event, _, control_flow| match event {
         Event::WindowEvent {
